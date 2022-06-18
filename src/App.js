6fd7b101c15f467418aedd4a5767ad9e013733ce
nawresgh <nawresgressi95@gmail.com>
2022-06-19 00:16:25 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css';
+import UserList from './Component/UserList/UserList';
+
+
+function App() {
+  const [listOfUsers, setListOfUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+      const getUsers  = async()=> {
+          try {
+            const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+            setListOfUsers(response.data);
+            console.log(response)
+            setLoading(false)
+          } catch (error) {
+            console.error(error);
+          }
+        }
+        useEffect(() => {
+          getUsers()
+        }, [])
+        
+  return (
+    <div className="App">
+     {loading? "Loading ..." :listOfUsers.map(el=> <UserList key={el.id} listOfUsers= {listOfUsers}/>)}
+    </div>
+  );
+}
+
+export default App;
